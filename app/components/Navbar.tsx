f5c4e12b8522360a98d2e0bcc77703ad06b80e35
cheesynamehere <156956223+cheesynamehere@users.noreply.github.com>
2025-02-23 21:08:16 -0800

    third push

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
         <div className="absolute flex flex-row w-screen h-12 justify-end items-center gap-3 bg-[#C14600]">
-            <p className="bg-[#E5D0AC] hover:bg-[#6f695d] transition-all duration:300 py-1 px-3 rounded-md text-black">Home</p>
+            <Link href="/"><p className="bg-[#E5D0AC] hover:bg-[#6f695d] transition-all duration:300 py-1 px-3 rounded-md text-black">Home</p></Link>
             <p className="bg-[#E5D0AC] hover:bg-amber-800 transition-all duration:300 py-1 px-3 rounded-md text-black">Projects</p>
             <p className="bg-[#E5D0AC] hover:bg-amber-800 transition-all duration:300 py-1 px-3 rounded-md">Contact</p>
             <p className="bg-[#E5D0AC] hover:bg-amber-800 transition-all duration:300 py-1 px-3 rounded-md">Recs</p>
+            <Link href="/resume"><p className="bg-[#E5D0AC] hover:bg-amber-800 transition-all duration:300 py-1 px-3 rounded-md">Resume</p></Link>
         </div>
     );
 }
---
+++ app/resume/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="pt-12 h-screen">
+            <iframe src="/assets/resume.pdf" className="w-full h-full object-cover" />
+        </div>
+    );
+  }
